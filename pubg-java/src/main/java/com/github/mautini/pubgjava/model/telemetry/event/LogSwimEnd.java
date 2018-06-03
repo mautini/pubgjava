3f9e1d1909785bb5d03d12a81d1cff629414eae8
mautini <mautini@users.noreply.github.com>
2018-06-04 00:16:46 +0200

    [PC] added LogSwimStart and LogSwimEnd telemetry events

---
+++ pubg-java/src/main/java/com/github/mautini/pubgjava/model/telemetry/event/LogSwimEnd.java
@@ -0,0 +1,8 @@
+package com.github.mautini.pubgjava.model.telemetry.event;
+
+public class LogSwimEnd extends TelemetryEventCharacter {
+
+    public LogSwimEnd() {
+        super();
+    }
+}
---
+++ pubg-java/src/main/java/com/github/mautini/pubgjava/model/telemetry/event/LogSwimStart.java
@@ -0,0 +1,8 @@
+package com.github.mautini.pubgjava.model.telemetry.event;
+
+public class LogSwimStart extends TelemetryEventCharacter {
+
+    public LogSwimStart() {
+        super();
+    }
+}
