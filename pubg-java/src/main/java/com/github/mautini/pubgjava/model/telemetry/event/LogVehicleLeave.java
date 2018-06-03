df9b65ba85d4bb350c6876789186581ec88121a3
mautini <mautini@users.noreply.github.com>
2018-06-04 00:21:52 +0200

    [PC] added rideDistance and seatIndex fields to LogVehicleLeave telemetry events

--- pubg-java/src/main/java/com/github/mautini/pubgjava/model/telemetry/event/LogVehicleLeave.java
+++ pubg-java/src/main/java/com/github/mautini/pubgjava/model/telemetry/event/LogVehicleLeave.java
@@ -1,8 +1,34 @@
 package com.github.mautini.pubgjava.model.telemetry.event;
 
+import com.google.gson.annotations.SerializedName;
+
 public class LogVehicleLeave extends TelemetryEventCharacterVehicle {
+
+    // PC Only
+    @SerializedName("rideDistance")
+    private Float rideDistance;
+
+    // PC Only
+    @SerializedName("seatIndex")
+    private Integer seatIndex;
 
     public LogVehicleLeave() {
         super();
+    }
+
+    public Float getRideDistance() {
+        return rideDistance;
+    }
+
+    public void setRideDistance(Float rideDistance) {
+        this.rideDistance = rideDistance;
+    }
+
+    public Integer getSeatIndex() {
+        return seatIndex;
+    }
+
+    public void setSeatIndex(Integer seatIndex) {
+        this.seatIndex = seatIndex;
     }
 }
