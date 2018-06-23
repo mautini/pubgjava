8640b2f28b23ff93456c834d1f6b80a424fb6ec5
mautini <mautini@users.noreply.github.com>
2018-06-23 11:59:09 +0200

    Add serialized name for shards

--- pubg-java/src/main/java/com/github/mautini/pubgjava/model/Shard.java
+++ pubg-java/src/main/java/com/github/mautini/pubgjava/model/Shard.java
@@ -1,20 +1,22 @@
 package com.github.mautini.pubgjava.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum Shard {
-    XBOX_AS("xbox-as"),
-    XBOX_EU("xbox-eu"),
-    XBOX_NA("xbox-na"),
-    XBOX_OC("xbox-oc"),
-    PC_KRJP("pc-krjp"),
-    PC_JP("pc-jp"),
-    PC_NA("pc-na"),
-    PC_EU("pc-eu"),
-    PC_RU("pc-ru"),
-    PC_OC("pc-oc"),
-    PC_KAKAO("pc-kakao"),
-    PC_SEA("pc-sea"),
-    PC_SA("pc-sa"),
-    PC_AS("pc-as");
+    @SerializedName("xbox-as") XBOX_AS("xbox-as"),
+    @SerializedName("xbox-eu") XBOX_EU("xbox-eu"),
+    @SerializedName("xbox-na") XBOX_NA("xbox-na"),
+    @SerializedName("xbox-oc") XBOX_OC("xbox-oc"),
+    @SerializedName("pc-krip") PC_KRJP("pc-krjp"),
+    @SerializedName("pc-jp") PC_JP("pc-jp"),
+    @SerializedName("pc-na") PC_NA("pc-na"),
+    @SerializedName("pc-eu") PC_EU("pc-eu"),
+    @SerializedName("pc-ru") PC_RU("pc-ru"),
+    @SerializedName("pc-oc") PC_OC("pc-oc"),
+    @SerializedName("pc-kakao") PC_KAKAO("pc-kakao"),
+    @SerializedName("pc-sea") PC_SEA("pc-sea"),
+    @SerializedName("pc-sa") PC_SA("pc-sa"),
+    @SerializedName("pc-as") PC_AS("pc-as");
 
     private final String text;
 
