3f147474fdf6f285e01d9cb85803b31f7936ff36
mautini <mautini@users.noreply.github.com>
2018-09-01 16:09:06 +0200

    Add GameMode for custom matches - compliant with API V5.0.0

--- pubg-java/src/main/java/com/github/mautini/pubgjava/model/GameMode.java
+++ pubg-java/src/main/java/com/github/mautini/pubgjava/model/GameMode.java
@@ -9,4 +9,10 @@ public enum GameMode {
     @SerializedName("solo-fpp") SOLO_FPP,
     @SerializedName("squad") SQUAD,
     @SerializedName("squad-fpp") SQUAD_FPP,
+    @SerializedName("normal-duo") NORMAL_DUO,
+    @SerializedName("normal-duo-fpp") NORMAL_DUO_FPP,
+    @SerializedName("normal-solo") NORMAL_SOLO,
+    @SerializedName("normal-solo-fpp") NORMAL_SOLO_FPP,
+    @SerializedName("normal-squad") NORMAL_SQUAD,
+    @SerializedName("normal-squad-fpp") NORMAL_SQUAD_FPP,
 }
