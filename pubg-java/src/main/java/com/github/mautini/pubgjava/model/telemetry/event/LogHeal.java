9bd300ca1ba7b8b090aa04560cf6b8b3ca5b4601
Nicolas Mauti <mautini@users.noreply.github.com>
2019-01-14 20:59:28 +0100

    Add missing line and extend TelemetryEventCharacterItem in LogHeal

--- pubg-java/src/main/java/com/github/mautini/pubgjava/model/telemetry/event/LogHeal.java
+++ pubg-java/src/main/java/com/github/mautini/pubgjava/model/telemetry/event/LogHeal.java
@@ -1,26 +1,15 @@
 package com.github.mautini.pubgjava.model.telemetry.event;
 
-import com.github.mautini.pubgjava.model.telemetry.object.Item;
+public class LogHeal extends TelemetryEventCharacterItem {
 
-public class LogHeal extends TelemetryEventCharacter {
-
-    private Item item;
     private float healAmount;
 
     public LogHeal() {
         super();
     }
 
-    public Item getItem() {
-        return item;
-    }
-
     public float getHealAmount() {
         return healAmount;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
     }
 
     public void setHealAmount(float healAmount) {
